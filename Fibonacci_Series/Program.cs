4e4e1591cd745e80b545efcad27f7723fa4923fc
solevivek5 <solevivek.singh5@gmail.com>
2024-06-23 23:16:24 +0530

    init

---
+++ Fibonacci_Series/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fibonacci_Series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.Write(  "Enter the Number: ");
+            number = int.Parse(Console.ReadLine() );
+            Console.Write(n1 + " " + n2 + " ");
+            for ( i = 0; i < number; ++i )
+            {
+                n3 = n1 + n2;
+                Console.Write(n3+ " ");
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
